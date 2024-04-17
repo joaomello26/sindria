bfa5d6bfa0542657df51a67e5d32202ce554af5f
joaomello <joaovictorkerber@gmail.com>
2024-04-16 21:24:34 -0300

    feat: add exception handling

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-from bs4 import BeautifulSoup
+import logging
 import requests
 import re
 import math
+import sys
+from bs4 import BeautifulSoup
 from models.connection_options.connection import DBConnectionHandler
 from models.repository.estuda_repository import EstudaRepository
 from selenium_actions import SeleniumAutomation
@@ -12,19 +14,40 @@ def connect_to_database():
     return db_handle.get_db_connection()
 
 def get_soup(url):
-    response = requests.get(url)
-    return BeautifulSoup(response.text, 'lxml')
+    # Fetches and parses html content from a given url using BeautifulSoup.
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raises HTTPError for bad HTTP responses
+    except requests.RequestException as e:
+        # Handle HTTP and other network-related errors
+        logging.error(f'Error fetching URL {url}: {e}')
+        return None
+    
+    soup = BeautifulSoup(response.text, 'lxml')
+    if soup is not None:
+        return soup
+    else:
+        # If lxml parsing results in an empty soup
+        logging.error(f'Failed to get any content from {url}.')
+        sys.exit(1)
 
 def get_total_pages(soup):
+    # Determines the total number of pages of exam database
     pagination_select = soup.find('li', class_='paginacao_paginas') 
     options = pagination_select.find_all('option')
     return int(options[-1].text)
 
 def fetch_all_exams(base_url, total_pages):
+    # Fetch all the exams in the site database
     exams = []
 
     for actual_page in range(1, total_pages + 1):
         page_soup = get_soup(f'{base_url}questoes_provas/?inicio={actual_page}')
+
+        if not page_soup: # Check if soup is None and exit if true
+            logging.error("Failed to get the exam database page content.")
+            sys.exit(1)
+
         exams.extend(get_exam_elements(page_soup))
 
     return exams
@@ -33,33 +56,39 @@ def get_exam_elements(page_soup):
     return page_soup.find_all('div', class_='col-md-4 col-sm-6 col-xs-12')
 
 def get_exam_details(exam):
+    # Extract exam details based on the name pattern
     exam_data = exam.find('span', class_='btn-block').get_text(strip=True)
     
     data_pattern = r'^([a-zA-Z ]+) (\d{4})(?:/(\d+))?(?:\s+.*)?$'
     match = re.search(data_pattern, exam_data.strip())
+
+    questions_number = int(exam.find('strong').find_next('strong').text)
 
     exam_details = {
         'name': match.group(1).strip(), 
         'year': int(match.group(2)),
         'number': int(match.group(3)) if match.group(3) else '',
         'exam_type': 'Multiple Choice',
-        'questions_number' : None
+        'questions_number' : questions_number
     }
 
-    # Get exam id
+    return exam_details
+
+def get_exam_id(exam):
+    # Extract the exam id based on the url pattern
     exam_relative_url = exam.find('a', class_='btn btn-success')['href']
     exam_id = re.search(r'prova=(\d+)&q', exam_relative_url).group(1)
 
-    return exam_details, exam_id
+    return exam_id
 
 def fetch_questions_for_exam(base_url, exam_id, exam_total_questions, bot):
-    questions_by_page = 6
+    # Extract the html content of all exam pages
+    questions_by_page = 6 # Site pattern
     exam_pages = math.ceil(exam_total_questions / questions_by_page)
     questions = []
 
-    # Iterate through each exam page
     for page_number in range(1, exam_pages+1):
-        # Use bot to answer the questions to extract the correct answer and questions topics
+        # Use bot to extract questions topics and answer the questions to extract the correct answer
         page_url = f'{base_url}questoes/?prova={exam_id}&inicio={page_number}'
         bot.driver.get(page_url)
 
@@ -68,6 +97,11 @@ def fetch_questions_for_exam(base_url, exam_id, exam_total_questions, bot):
         page_content = bot.driver.page_source
         
         page_soup = BeautifulSoup(page_content, 'lxml')
+
+        if not page_soup:
+            logging.error('Failed to get questions content.')
+            sys.exit(1)
+
         questions.extend(get_question_elements(page_soup))
 
     return questions
@@ -75,11 +109,11 @@ def fetch_questions_for_exam(base_url, exam_id, exam_total_questions, bot):
 def get_question_elements(exam_page_soup):
     return exam_page_soup.find_all('div', id=re.compile('^d_questao'))
 
-def extract_question_content(question):    
-    question_statemennt = extract_question_statement(question)
-    question_topics = extract_question_topics(question)
-    [question_alternatives, correct_aswer] = extract_question_alternatives(question)
-    [question_id, question_difficulty] = extract_question_data(question)
+def assemble_question_content(question):    
+    question_statemennt = get_question_statement(question)
+    question_topics = get_question_topics(question)
+    [question_alternatives, correct_aswer] = get_question_alternatives(question)
+    [question_id, question_difficulty] = get_question_data(question)
 
     question_content = {
         'statement': question_statemennt,
@@ -91,24 +125,25 @@ def extract_question_content(question):
 
     return question_content, question_id
 
-def extract_question_topics(question):
+def get_question_topics(question):
+    # Extract the question subject and related topics of the question
     tags_elements = question.find('ul', class_='list-tags')
 
-    subject = tags_elements.find('li', class_='active').get_text(strip=True)
+    question_subject = tags_elements.find('li', class_='active').get_text(strip=True)
 
     topic_elements = tags_elements.find_all('a', target='_top')
-    topics = []
+    question_topics = []
     for topic in topic_elements:
-        topics.append(topic.get_text(strip=True))
+        question_topics.append(topic.get_text(strip=True))
 
     question_topics = {
-        'subject': subject,
-        'path': {'tree': topics}
+        'subject': question_subject,
+        'path': {'tree': question_topics}
     }
 
     return question_topics
 
-def extract_question_alternatives(question):
+def get_question_alternatives(question):
     label_class = 'check btn-block d-flex justify-content-between'
 
     alternative_elements = question.find_all('label', class_=re.compile(f'^{label_class}'))
@@ -140,7 +175,8 @@ def extract_question_alternatives(question):
 
     return alternatives, correct_answer
 
-def extract_question_data(question):
+def get_question_data(question):
+    # Extract the question data based on name pattern
     question_info = question.find('div', class_='panel-title-box').find('h3').text.split()
 
     question_id = question_info[2]
@@ -148,7 +184,7 @@ def extract_question_data(question):
 
     return question_id, question_difficulty
 
-def extract_question_statement(question):
+def get_question_statement(question):
     [question_prompt_text, question_query_text] = get_full_statement_text(question)
 
     # Extract full statement info
@@ -216,12 +252,19 @@ def get_full_statement_text(question):
     return question_prompt_text, question_query_text
     
 def main():
+    logging.info('Starting the web scraping process.')
+
     # Connect do Database
     db_connection = connect_to_database()
     estuda_repository = EstudaRepository(db_connection)
 
     base_url = 'https://app.estuda.com/'
     soup = get_soup(base_url + 'questoes_provas')
+
+    if not soup: # Check if soup is None and exit if true
+        logging.error('Failed to retrieve the initial page for scraping. Exiting program.')
+        sys.exit(1) 
+
     total_pages = get_total_pages(soup)
 
     # Initialize Selenium Automation
@@ -229,22 +272,26 @@ def main():
     bot.login()
 
     exams = fetch_all_exams(base_url, total_pages)
+    logging.info('Fetch exams successfull')
         
-    # Iterate through each exam to fetch details and questions
+    # Iterate through each exam to extract details and questions
     for exam in exams:
-        [exam_details, exam_id] = get_exam_details(exam)
+        exam_details = get_exam_details(exam)
+        exam_id = get_exam_id(exam)
 
         exam_total_questions = exam_details['questions_number']
         questions = fetch_questions_for_exam(base_url, exam_id, exam_total_questions, bot)
         
         for question in questions:
-            [question_content, question_id] = extract_question_content(question)
+            [question_content, question_id] = assemble_question_content(question)
             question_data = {
                 'id': question_id,
                 'exam_details': exam_details,
                 'question_content': question_content
             }
             estuda_repository.insert_document(question_data)
+
+        logging.info(f'Extract all question from {exam_details['name']} {exam_details['year']} {exam_details['number']}successfull')
 
 if __name__ == '__main__':
     main()
--- selenium_actions.py
+++ selenium_actions.py
@@ -1,16 +1,18 @@
 import os
 import time
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 class SeleniumAutomation:
     def __init__(self, driver_path="C:/SeleniumDrivers", headless=False):
         self.driver = self.configure_driver(driver_path, headless)
 
     def __exit__(self):
-        self.quit()
+        self.driver.quit()
 
     def configure_driver(self, driver_path, headless):
         os.environ['PATH'] += driver_path
@@ -23,27 +25,62 @@ class SeleniumAutomation:
         driver = webdriver.Chrome(options=options)
         driver.maximize_window()
         return driver
+    
+    def navigate_to_page(self, url):
+        try:
+            self.driver.get(url)
+            logging.info(f'Successfully navigated to {url}')
+        except Exception as e:
+            logging.error(f'Failed to navigate to {url} due to {e}')
 
     def login(self):
         login_url = 'https://app.estuda.com/usuarios_login'
         email = 'alexdubugras@gmail.com'  
         password = 'sindria123'
 
-        self.driver.get(login_url)
+        self.navigate_to_page(login_url)
 
-        WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.NAME, 'siu_email'))
-        )
-        self.driver.find_element(By.NAME, 'siu_email').send_keys(email)
-        self.driver.find_element(By.NAME, 'siu_senha').send_keys(password)
-        self.driver.find_element(By.CSS_SELECTOR, '.btn.btn-info.btn-lg.btn-block.text-uppercase').click()
+        try:
+            # Ensure the email input is clickable and ready for input
+            email_input = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.NAME, 'siu_email'))
+            )
+            email_input.send_keys(email)  # Send email to the input field
+
+            # Input password
+            password_input = self.driver.find_element(By.NAME, 'siu_senha')
+            password_input.send_keys(password)
+
+            # Click the login button
+            login_button = self.driver.find_element(By.CSS_SELECTOR, '.btn.btn-info.btn-lg.btn-block.text-uppercase')
+            login_button.click()
+
+            logging.info('Login successful')
+        except TimeoutException:
+            logging.warning('Element not clickable within timeout period.')
+        except NoSuchElementException:
+            logging.error('Element not found on the page.')
+        except Exception as e:
+            logging.error(f'An unexpected error occurred: {e}')
 
     def click_elements(self, elements_xpath):
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, elements_xpath)))
-        elements = self.driver.find_elements(By.XPATH, elements_xpath)
-        for element in elements:
-            self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
-            element.click()
+        try:
+            # Ensure the element is clickable
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, elements_xpath))
+            )
+
+            elements = self.driver.find_elements(By.XPATH, elements_xpath)
+            for element in elements:
+                self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
+                element.click()
+
+        except TimeoutException:
+            logging.warning('Element not clickable within timeout period.')
+        except NoSuchElementException:
+            logging.error('Element not found on the page.')
+        except Exception as e:
+            logging.error(f'An unexpected error occurred: {e}')
 
     def get_answers(self):
         alternative_xpath = '//div[@class=\'respostas form form-group\']/label[1]'
